b1b676035ede87bd64fd49fb554f5f7033f96ce1
Maoyuan <>
2023-03-30 09:56:43 -0400

    view score history btn

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ currentgrade = 0;
 var nowtimenum;
 nowtimenum = 60;
 var timer;
+
+document.querySelector("#title").addEventListener("click", function () {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("highscorepage").style.display = "block";
+});
